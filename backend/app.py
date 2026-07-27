ef1c814951ea25a129a93021668131febcd0cc1d
karansuyal <149187681+karansuyal@users.noreply.github.com>
2026-07-27 12:54:25 +0530

    Fix Cloudinary import

--- backend/app.py
+++ backend/app.py
@@ -13,7 +13,7 @@ from dotenv import load_dotenv
 from .cloudinary_config import configure_cloudinary
 import resend
 
-from extensions import db, jwt, mail, oauth, limiter
+from .extensions import db, jwt, mail, oauth, limiter
 from models import User, Course, Subject, Note
 
 load_dotenv()
